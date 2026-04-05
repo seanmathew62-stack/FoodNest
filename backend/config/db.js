783f3e252809124234239a9819155f840479e685
parthkpatil123-gif <parthkpatil123@gmail.com>
2026-04-05 19:58:17 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,12 @@
+const oracledb = require("oracledb");
+
+async function getConnection() {
+  return oracledb.getConnection({
+    user: "YOUR_USERNAME",
+    password: "YOUR_PASSWORD",
+    connectionString: "localhost:1521/XEPDB1"
+  });
+}
+
+module.exports = { getConnection };
 
